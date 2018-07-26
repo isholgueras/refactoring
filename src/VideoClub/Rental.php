252a2c160b142a6df50b147f799240c22b9a15c0
Ignacio Sánchez <chet161984@gmail.com>
2018-07-27 00:39:39 +0200

    Original

---
+++ src/VideoClub/Rental.php
@@ -0,0 +1,25 @@
+<?php
+
+class Rental
+{
+    /** @var Movie */
+    private $movie;
+
+    private $daysRented;
+
+    public function __construct(Movie $movie, $daysRented)
+    {
+        $this->daysRented = $daysRented;
+        $this->movie = $movie;
+    }
+
+    public function getDaysRented()
+    {
+        return $this->daysRented;
+    }
+
+    public function getMovie()
+    {
+        return $this->movie;
+    }
+}
